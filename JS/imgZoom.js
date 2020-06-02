ab12a08719190a90d3aca62152b3da4a68c1f34e
xianbin <1169411465@qq.com>
2020-06-02 10:48:53 +0800

    updata:base v0.3

--- JS/imgZoom.js
+++ JS/imgZoom.js
@@ -1,202 +1,239 @@
-var imgZoom=(function(){
-  let img={
-    width:1000,
+"use strict";
+var imgZoom = (function(){
+  function isPE() {
+    return /Android|webOS|iPhone|ipod|ipad|BlackBerry/i.test(navigator.userAgent);
   }
-  function create(url){
-    let body=document.getElementsByTagName('body')[0]
-    let imgb=document.createElement('div')
-    body.appendChild(imgb)
-    imgb.innerHTML='<div id="_imgzoom" style="width:'+img.width+'px;position:fixed;top: 460px;left:50%;transform: translate(-50%,-50%);background:#fff;z-index: 1;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select:       none;padding:5px 10px 25px;">\
-        <div class="header" style="">\
-            <p style="margin:0;padding:8px 0">滚动滚轮缩放图片\
-              <span style="float:right">\
-                <a id="imgzoom_imglink" class="imglink" target="_blank" title="在新窗口打开" href="'+url+'" style="background: url(data:image/gif;base64,R0lGODlhYQA4AKIAAAAAAP///0SXzJmZmf///wAAAAAAAAAAACH5BAEAAAQALAAAAABhADgAAAP/SLo7/o7JSRW8uOqKeyQdGG6k6I2ldq4ZA6XbR8rw9MyX+tZURDcDEa8XLO2Iwd9QWOwJl65m7Ia0SKFKIjQK7Fqz1OdWjMryZLfwkesdo1viLS0HNI919aMd9gt/r1p3ZHB7KXNraxyAS290cVhNaY2LEoWGlJWYfJGceUaamyAqj4ygf5+CTIs+pjiXfjaWqLNuhSweqbeOcrqSt6l/LMC9rcDGx8jJysvMzc7P0NHS09TV1tfY2drb3N3e3+Dh4uPk5ebn6Onq6+zt7u/w8TwC9PX0qfb5+sD6/fcE/QAGvOOvoKCCCPcxsHdQQIp/Y+qRUEghX0OBFR1C3LJRkAPDChI7LrnnECRGgiUnSsxYUuQQlxNg1oBIM+VHCStPRrRJUSaMmhqBplw41OdDnhYVGC2xManSnM+E5lyqEmfHmx6HckR6k+qGplixgtQKRWrNi0+DcmVKdmTbmG9n8lwglC0+jR51bj261OvXoBn1lo1LFCpcwj8RP22IOKG/u44Nu41MMrIxyor5OiaQAAA7) no-repeat 0 0;width:17px;height:17px;float:left;margin-left:10px;overflow:hidden;line-height:100px">在新窗口打开</a>\
-                <a id="imgzoom_adjust" onclick="imgZoom.resize()" href="javascipt:;" class="imgadjust" title="优化显示" style="background: url(data:image/gif;base64,R0lGODlhYQA4AKIAAAAAAP///0SXzJmZmf///wAAAAAAAAAAACH5BAEAAAQALAAAAABhADgAAAP/SLo7/o7JSRW8uOqKeyQdGG6k6I2ldq4ZA6XbR8rw9MyX+tZURDcDEa8XLO2Iwd9QWOwJl65m7Ia0SKFKIjQK7Fqz1OdWjMryZLfwkesdo1viLS0HNI919aMd9gt/r1p3ZHB7KXNraxyAS290cVhNaY2LEoWGlJWYfJGceUaamyAqj4ygf5+CTIs+pjiXfjaWqLNuhSweqbeOcrqSt6l/LMC9rcDGx8jJysvMzc7P0NHS09TV1tfY2drb3N3e3+Dh4uPk5ebn6Onq6+zt7u/w8TwC9PX0qfb5+sD6/fcE/QAGvOOvoKCCCPcxsHdQQIp/Y+qRUEghX0OBFR1C3LJRkAPDChI7LrnnECRGgiUnSsxYUuQQlxNg1oBIM+VHCStPRrRJUSaMmhqBplw41OdDnhYVGC2xManSnM+E5lyqEmfHmx6HckR6k+qGplixgtQKRWrNi0+DcmVKdmTbmG9n8lwglC0+jR51bj261OvXoBn1lo1LFCpcwj8RP22IOKG/u44Nu41MMrIxyor5OiaQAAA7) no-repeat -40px 0;width:17px;height:17px;float:left;margin-left:10px;overflow:hidden;line-height:100px">优化显示</a>\
-                <a href="javascript:;" onclick="imgZoom.hideMenu()" class="imgclose" title="关闭" style="background: url(data:image/gif;base64,R0lGODlhYQA4AKIAAAAAAP///0SXzJmZmf///wAAAAAAAAAAACH5BAEAAAQALAAAAABhADgAAAP/SLo7/o7JSRW8uOqKeyQdGG6k6I2ldq4ZA6XbR8rw9MyX+tZURDcDEa8XLO2Iwd9QWOwJl65m7Ia0SKFKIjQK7Fqz1OdWjMryZLfwkesdo1viLS0HNI919aMd9gt/r1p3ZHB7KXNraxyAS290cVhNaY2LEoWGlJWYfJGceUaamyAqj4ygf5+CTIs+pjiXfjaWqLNuhSweqbeOcrqSt6l/LMC9rcDGx8jJysvMzc7P0NHS09TV1tfY2drb3N3e3+Dh4uPk5ebn6Onq6+zt7u/w8TwC9PX0qfb5+sD6/fcE/QAGvOOvoKCCCPcxsHdQQIp/Y+qRUEghX0OBFR1C3LJRkAPDChI7LrnnECRGgiUnSsxYUuQQlxNg1oBIM+VHCStPRrRJUSaMmhqBplw41OdDnhYVGC2xManSnM+E5lyqEmfHmx6HckR6k+qGplixgtQKRWrNi0+DcmVKdmTbmG9n8lwglC0+jR51bj261OvXoBn1lo1LFCpcwj8RP22IOKG/u44Nu41MMrIxyor5OiaQAAA7) no-repeat -80px 0;width:17px;height:17px;float:left;margin-left:10px;overflow:hidden;line-height:100px">关闭</a>\
-              </span>\
-            </p>\
-        </div>\
-        <div style="position:relative;">\
-          <div class="last" style="display: block;position: absolute;width: 80px;height: 100%;left:0;cursor: pointer;">\
-              <p style="background:#fff;color:#000;position:absolute;top:40%;left:0;margin:0;padding:5px;font-size:12px;display:none">上一张</p>\
-          </div>  \
-          <div class="next" style="display: block;position: absolute;width: 80px;height: 100%;right:0;cursor: pointer;">\
-              <p style="background:#fff;color:#000;position:absolute;top:40%;right:0;margin:0;padding:5px;font-size:12px;display:none">下一张</p>\
-          </div>\
-          <img id="net" src="'+url+'" oncopy="return false;" style="width:100%;vertical-align: top;">\
-        </div>\
-    </div>\
-    <div class="shadow" onclick="imgZoom.hideMenu()" style="position: absolute;width: 100%;height: 100%;background-color: rgb(0, 0, 0);top:0;left:0;opacity: 0.5;"/></div>'
-    let shadow=document.getElementsByClassName('shadow')[0]
-    shadow.style.height=document.documentElement.offsetHeight+'px'
-  }
-  function open(event){
-    if(event&&event.src){
-    let imgz=document.getElementById('_imgzoom')
-    let imgs=document.getElementsByClassName('imgzoom')
-    if(!imgz&&imgs.length>0){
-      create(event.src)
-      bing()
-    }else{
-      let imgz=document.getElementById('_imgzoom')
-      let dom1=document.getElementById('net')
-      let dom2=document.getElementById('imgzoom_imglink')
-      dom1.src=event.src
-      dom2.href=event.src
-      imgz.parentElement.style.display="block"
-      imgz.style.top="50%"
-      imgz.style.left="50%"
-      imgz.style.width=img.width+'px'
+
+  //创建函数
+  function create(url) {
+    var imgwidth = img.width;
+
+    if (img.width == "auto") {
+      imgwidth = 1000;
     }
-    }else{
-      console.log('缺少参数 src:Missing parameters "src"')
+
+    var body = document.getElementsByTagName('body')[0];
+    var imgb = document.createElement('div');
+    body.appendChild(imgb);
+    imgb.innerHTML = '<div id="_imgzoom" style="width:' + imgwidth + 'px;position:fixed;top:50%;left:50%;transform: translate(-50%,-50%);background:#fff;z-index: 1;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select:       none;padding:5px 10px 25px;"><div class="header" style=""><p style="margin:0;padding:8px 0">滚动滚轮缩放图片<span style="float:right"><a id="imgzoom_imglink" class="imgzoom_imglink" target="_blank" title="在新窗口打开" href="' + url + '" style="background: url(data:image/gif;base64,R0lGODlhYQA4AKIAAAAAAP///0SXzJmZmf///wAAAAAAAAAAACH5BAEAAAQALAAAAABhADgAAAP/SLo7/o7JSRW8uOqKeyQdGG6k6I2ldq4ZA6XbR8rw9MyX+tZURDcDEa8XLO2Iwd9QWOwJl65m7Ia0SKFKIjQK7Fqz1OdWjMryZLfwkesdo1viLS0HNI919aMd9gt/r1p3ZHB7KXNraxyAS290cVhNaY2LEoWGlJWYfJGceUaamyAqj4ygf5+CTIs+pjiXfjaWqLNuhSweqbeOcrqSt6l/LMC9rcDGx8jJysvMzc7P0NHS09TV1tfY2drb3N3e3+Dh4uPk5ebn6Onq6+zt7u/w8TwC9PX0qfb5+sD6/fcE/QAGvOOvoKCCCPcxsHdQQIp/Y+qRUEghX0OBFR1C3LJRkAPDChI7LrnnECRGgiUnSsxYUuQQlxNg1oBIM+VHCStPRrRJUSaMmhqBplw41OdDnhYVGC2xManSnM+E5lyqEmfHmx6HckR6k+qGplixgtQKRWrNi0+DcmVKdmTbmG9n8lwglC0+jR51bj261OvXoBn1lo1LFCpcwj8RP22IOKG/u44Nu41MMrIxyor5OiaQAAA7) no-repeat 0 0;width:17px;height:17px;float:left;margin-left:10px;overflow:hidden;line-height:100px">在新窗口打开</a><a id="imgzoom_resize" class="imgzoom_resize" href="javascipt:;" title="优化显示" style="background: url(data:image/gif;base64,R0lGODlhYQA4AKIAAAAAAP///0SXzJmZmf///wAAAAAAAAAAACH5BAEAAAQALAAAAABhADgAAAP/SLo7/o7JSRW8uOqKeyQdGG6k6I2ldq4ZA6XbR8rw9MyX+tZURDcDEa8XLO2Iwd9QWOwJl65m7Ia0SKFKIjQK7Fqz1OdWjMryZLfwkesdo1viLS0HNI919aMd9gt/r1p3ZHB7KXNraxyAS290cVhNaY2LEoWGlJWYfJGceUaamyAqj4ygf5+CTIs+pjiXfjaWqLNuhSweqbeOcrqSt6l/LMC9rcDGx8jJysvMzc7P0NHS09TV1tfY2drb3N3e3+Dh4uPk5ebn6Onq6+zt7u/w8TwC9PX0qfb5+sD6/fcE/QAGvOOvoKCCCPcxsHdQQIp/Y+qRUEghX0OBFR1C3LJRkAPDChI7LrnnECRGgiUnSsxYUuQQlxNg1oBIM+VHCStPRrRJUSaMmhqBplw41OdDnhYVGC2xManSnM+E5lyqEmfHmx6HckR6k+qGplixgtQKRWrNi0+DcmVKdmTbmG9n8lwglC0+jR51bj261OvXoBn1lo1LFCpcwj8RP22IOKG/u44Nu41MMrIxyor5OiaQAAA7) no-repeat -40px 0;width:17px;height:17px;float:left;margin-left:10px;overflow:hidden;line-height:100px">优化显示</a><a id="imgzoom_close" class="imgzoom_close" href="javascript:;" title="关闭" style="background: url(data:image/gif;base64,R0lGODlhYQA4AKIAAAAAAP///0SXzJmZmf///wAAAAAAAAAAACH5BAEAAAQALAAAAABhADgAAAP/SLo7/o7JSRW8uOqKeyQdGG6k6I2ldq4ZA6XbR8rw9MyX+tZURDcDEa8XLO2Iwd9QWOwJl65m7Ia0SKFKIjQK7Fqz1OdWjMryZLfwkesdo1viLS0HNI919aMd9gt/r1p3ZHB7KXNraxyAS290cVhNaY2LEoWGlJWYfJGceUaamyAqj4ygf5+CTIs+pjiXfjaWqLNuhSweqbeOcrqSt6l/LMC9rcDGx8jJysvMzc7P0NHS09TV1tfY2drb3N3e3+Dh4uPk5ebn6Onq6+zt7u/w8TwC9PX0qfb5+sD6/fcE/QAGvOOvoKCCCPcxsHdQQIp/Y+qRUEghX0OBFR1C3LJRkAPDChI7LrnnECRGgiUnSsxYUuQQlxNg1oBIM+VHCStPRrRJUSaMmhqBplw41OdDnhYVGC2xManSnM+E5lyqEmfHmx6HckR6k+qGplixgtQKRWrNi0+DcmVKdmTbmG9n8lwglC0+jR51bj261OvXoBn1lo1LFCpcwj8RP22IOKG/u44Nu41MMrIxyor5OiaQAAA7) no-repeat -80px 0;width:17px;height:17px;float:left;margin-left:10px;overflow:hidden;line-height:100px">关闭</a></span></p></div><div style="position:relative;"><div class="last" style="display: block;position: absolute;width: 80px;height: 100%;left:0;cursor: pointer;"><p style="background:#fff;color:#000;position:absolute;top:40%;left:0;margin:0;padding:5px;font-size:12px;display:none">上一张</p></div><div class="next" style="display: block;position: absolute;width: 80px;height: 100%;right:0;cursor: pointer;"><p style="background:#fff;color:#000;position:absolute;top:40%;right:0;margin:0;padding:5px;font-size:12px;display:none">下一张</p></div><img id="net" src="' + url + '" oncopy="return false;" style="width:100%;vertical-align: top;"></div></div><div class="imgzoom_shadow" style="position:fixed;width: 100%;height: 100%;background-color: rgb(0, 0, 0);top:0;left:0;opacity: 0.5;"/></div>';
+    
+    if (window.innerWidth <= 1024 || window.innerHeight <= 630) {
+
+      if (img.width == "auto") {
+        resize();
+      }
     }
   }
-  //绑定事件
-  function bing(){
-    lastnext('last')
-    lastnext('next')
-    mouseWheel()
-    imgMove()
-  }
-  //前进后退
-  function lastnext(name){
-    let dom=document.getElementById('_imgzoom').getElementsByClassName(name)[0]
-    dom.addEventListener('mousemove',function(){
-      dom.getElementsByTagName('p')[0].style.display="block";
-    })
-    dom.addEventListener('mouseout',function(){
-      dom.getElementsByTagName('p')[0].style.display="none"
-    })
-    dom.addEventListener('mousedown',function(event){
-      event.stopPropagation()
-    })
-    dom.addEventListener('click',function(event){
-      let imgurl=document.getElementById('net').src
-      let arrs=document.getElementsByClassName('imgzoom')
-      for(let i=0;i<arrs.length;i++){
-        if(name=='next'){
-          if(arrs[i].src==imgurl&&i<arrs.length-1){
-            document.getElementById('net').src=arrs[i+1].src
-            document.getElementById('imgzoom_imglink').href=arrs[i+1].src
-            break
-          }else{
-            document.getElementById('net').src=arrs[0].src
-            document.getElementById('imgzoom_imglink').href=arrs[0].src
+
+  function open(event) {
+    if (event && event.src) {
+      var imgz = document.getElementById('_imgzoom');
+      var imgs = document.getElementsByClassName('imgZoom');
+
+      if (!imgz && imgs.length > 0) {
+        create(event.src);
+        bing();
+      } else {
+        var _imgz = document.getElementById('_imgzoom');
+
+        var dom1 = document.getElementById('net');
+        var dom2 = document.getElementById('imgzoom_imglink');
+        dom1.src = event.src;
+        dom2.href = event.src;
+        _imgz.parentElement.style.display = "block";
+        _imgz.style.top = "50%";
+        _imgz.style.left = "50%";
+        _imgz.style.width = img.width + 'px';
+      }
+    } else {
+      console.error('缺少参数 src:Missing parameters "src"');
+    }
+  } //绑定事件
+
+
+  function bing() {
+    lastnext('last');
+    lastnext('next');
+    mouseWheel();
+    imgMove();
+  } //前进后退
+
+
+  function lastnext(name) {
+    var dom = document.getElementById('_imgzoom').getElementsByClassName(name)[0];
+    dom.addEventListener('mousemove', function () {
+      dom.getElementsByTagName('p')[0].style.display = "block";
+    });
+    dom.addEventListener('mouseout', function () {
+      dom.getElementsByTagName('p')[0].style.display = "none";
+    });
+    dom.addEventListener('mousedown', function (event) {
+      event.stopPropagation();
+    });
+    dom.addEventListener('click', function (event) {
+      var imgurl = document.getElementById('net').src;
+      var arrs = document.getElementsByClassName('imgZoom');
+
+      for (var i = 0; i < arrs.length; i++) {
+        if (name == 'next') {
+          if (arrs[i].src == imgurl && i < arrs.length - 1) {
+            document.getElementById('net').src = arrs[i + 1].src;
+            document.getElementById('imgzoom_imglink').href = arrs[i + 1].src;
+            break;
+          } else {
+            document.getElementById('net').src = arrs[0].src;
+            document.getElementById('imgzoom_imglink').href = arrs[0].src;
           }
-        }else{
-          if(arrs[i].src==imgurl&&i>0){
-            document.getElementById('net').src=arrs[i-1].src
-            document.getElementById('imgzoom_imglink').href=arrs[i-1].src
-            break
-          }else{
-            document.getElementById('net').src=arrs[arrs.length-1].src
-            document.getElementById('imgzoom_imglink').href=arrs[arrs.length-1].src              }
+        } else {
+          if (arrs[i].src == imgurl && i > 0) {
+            document.getElementById('net').src = arrs[i - 1].src;
+            document.getElementById('imgzoom_imglink').href = arrs[i - 1].src;
+            break;
+          } else {
+            document.getElementById('net').src = arrs[arrs.length - 1].src;
+            document.getElementById('imgzoom_imglink').href = arrs[arrs.length - 1].src;
+          }
         }
       }
-    })
-  }
-  //滚轮事件
-  function mouseWheel(){
-      let dom=document.getElementById('_imgzoom')
-      dom.addEventListener('mousewheel', function(event) {
-        event.preventDefault()
-        Detail(event,dom);
-      });
-      dom.addEventListener('DOMMouseScroll', function(event) {
-        event.preventDefault()
-        Detail(event,dom);
-      });
-  }
-  //放大缩小
-  function Detail(e,dom){
-    if(e.deltaY>0){
-      let w=Math.round(dom.offsetWidth*0.9)
-      let h=Math.round(dom.offsetHeight*0.9)
-      if(w>360||h>270){
-        dom.style.width=w+'px'
+    });
+  } //滚轮事件
+
+
+  function mouseWheel() {
+    var dom = document.getElementById('_imgzoom');
+    dom.addEventListener('mousewheel', function (event) {
+      event.preventDefault();
+      Detail(event, dom);
+    });
+    dom.addEventListener('DOMMouseScroll', function (event) {
+      event.preventDefault();
+      Detail(event, dom);
+    });
+  } //放大缩小
+
+
+  function Detail(e, dom) {
+    if (e.deltaY > 0) {
+      var w = Math.round(dom.offsetWidth * 0.9);
+      var h = Math.round(dom.offsetHeight * 0.9);
+
+      if (w > 360 || h > 270) {
+        dom.style.width = w + 'px';
       }
-    }else{
-      let w=Math.round(dom.offsetWidth*1.1)
-      let h=Math.round(dom.offsetHeight*1.1)
-      if(w<82500||h<46500){
-        dom.style.width=w+'px'
-      }
-    }
-  }
-  //优化显示：调整大小
-  function resize(){
-    let url=document.getElementById('net').src
-    let dom=document.getElementById('_imgzoom')
-    let img = new Image()
-    img.src = url
-    if(img.complete){
-      size()
-    }else{
-      img.onload = function(){
-        size()
+    } else {
+      var _w = Math.round(dom.offsetWidth * 1.1);
+
+      var _h = Math.round(dom.offsetHeight * 1.1);
+
+      if (_w < 82500 || _h < 46500) {
+        dom.style.width = _w + 'px';
       }
     }
-    function size(){
-      let imgz=document.getElementById('_imgzoom')
-      let x=window.innerWidth/window.innerHeight
-      let y=img.width/img.height
-      if(x>=y){
-        let num=(window.innerHeight-70)*img.width/img.height
-        dom.style.width=num+'px' 
-      }else{
-        dom.style.width=(window.innerWidth-40)+'px'  
+  } //优化显示：调整大小
+
+
+  function resize() {
+    var url = document.getElementById('net').src;
+    var dom = document.getElementById('_imgzoom');
+    var img = new Image();
+    img.src = url;
+
+    if (img.complete) {
+      size();
+    } else {
+      img.onload = function () {
+        size();
+      };
+    }
+
+    function size() {
+      var x = window.innerWidth / window.innerHeight;
+      var y = img.width / img.height;
+
+      if (x >= y) {
+        var num = (window.innerHeight - 70) * img.width / img.height;
+        dom.style.width = num + 'px';
+      } else {
+        dom.style.width = window.innerWidth - 40 + 'px';
       }
-      imgz.style.top="50%"
-      imgz.style.left="50%"
+
+      dom.style.top = "50%";
+      dom.style.left = "50%";
+    }
+  } //移动事件 
+
+
+  function imgMove() {
+    var dom = document.getElementById('_imgzoom');
+    var screenX, screenY;
+    dom.addEventListener('mousedown', function (e) {
+      e.preventDefault();
+      screenX = e.screenX;
+      screenY = e.screenY;
+      window.addEventListener('mousemove', position);
+    });
+    window.addEventListener('mouseup', function (e) {
+      window.removeEventListener('mousemove', position);
+    });
+
+    function position(e) {
+      var x = e.screenX - screenX;
+      var y = e.screenY - screenY;
+      screenX = e.screenX;
+      screenY = e.screenY;
+      var top = dom.offsetTop;
+      var left = dom.offsetLeft;
+      dom.style.top = top + y + 'px';
+      dom.style.left = left + x + 'px';
+    }
+  } //设置选项
+
+
+  function option(e) {
+    if (e && e.width) {
+      img.width = e.width;
+    }
+  } //关闭窗口
+
+
+  function hideMenu() {
+    var _imgzoom = document.getElementById('_imgzoom');
+
+    if (_imgzoom) {
+      _imgzoom.parentElement.style.display = "none";
     }
   }
-  //移动事件
-  function imgMove(){
-    let dom=document.getElementById('_imgzoom')
-    let screenX,screenY
-    dom.addEventListener('mousedown',function(e){
-      e.preventDefault()
-      screenX=e.screenX
-      screenY=e.screenY
-      window.addEventListener('mousemove',position)
-    })
-    window.addEventListener('mouseup',function(e){
-      window.removeEventListener('mousemove',position)
-    })
-    function position(e){
-      let x=e.screenX-screenX
-      let y=e.screenY-screenY
-      screenX=e.screenX
-      screenY=e.screenY
-      let top=dom.offsetTop
-      let left=dom.offsetLeft
-      dom.style.top=(top+y)+'px'
-      dom.style.left=(left+x)+'px'
+
+  var img = {
+    width: 'auto',
+    top: '50%',
+    left: '50%'
+  };
+
+  if (isPE()) {
+    console.warn('手机端兼容性未适配，可能出现未知错误！！');
+  }
+
+  document.addEventListener('click', function (event) {
+    var even = event || window.event;
+    var target = even.target || even.srcElement;
+
+    if (target.nodeName == "IMG" && target.className == "imgZoom") {
+      open(target);
     }
-  }
-  //设置选项
-  function option(e){
-    if(e&&e.width){
-      img.width=e.width
+
+    if (target.className == "imgzoom_shadow" || target.className == "imgzoom_close") {
+      hideMenu();
     }
-  }
-  //关闭窗口
-  function hideMenu(){
-    let _imgzoom=document.getElementById('_imgzoom')
-    if(_imgzoom){
-      _imgzoom.parentElement.style.display="none"
+
+    if (target.className == "imgzoom_resize") {
+      resize();
     }
-  }
-  //获取width
-  function getWidth(){
-    return img.width
-  }
-  return{ 
+  });
+  return{
+    option:img,
     open:open,
-    option:option,
     resize:resize,
-    getWidth:getWidth,
-    hideMenu:hideMenu,
+    close:hideMenu
   }
 })()
--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # imgZoom
-一个可以查看图片(可以缩放，拖拽)的JS插件
+一个可以查看图片(可以缩放，拖拽)的JS插件，无需引入额外 js 插件，简洁，方便，兼容
+![Image text](./Img/202006011.png?imageView/2/w/960/q/540)
+
+# 在线预览
+
+在线预览 https://lixianbin1.github.io/imgZoom/
+
+# 简单使用
+
+>下载 imgzoom.js 文件
+
+引入js文件
+```
+<script type="text/javascript" src="./imgZoom.min.js"></script>
+```
+
+在需要查看的图片上添加 class="imgZoom"
+```
+<img class="imgZoom" src="./Img/202001034.png" alt="示例图片" >
+```
+
+在 script 中使用，接下来就可以查看所有imgzoom图片了
+```
+var imgz=new imgZoom()
+```
+
+# 全局配置
+
+## width:string
+
+调整打开窗口的宽度
+
+示例：
+```
+const img=new imgZoom()
+let obj={
+  width:"1000"
+}
+img.option=obj
+```
+
+
+# 全局API
+
+## imgZoom.open
+
+>imgZoom.open(target)
+
+可以用于打开指定 img 图片；img可以无需指定 class
+
+示例：
+```
+const img=new imgZoom()
+const dome=document.getElementsByClassName('dome')[0]
+
+img.open(dome)
+```
+
+## imgZoom.resize
+
+>imgZoom.resize()
+
+可以用于调整打开窗口的大小。达到优化展示的效果
+
+示例：
+```
+const img=new imgZoom()
+
+img.resize()
+```
+
+## imgZoom.close
+
+>imgZoom.close()
+
+可以用于关闭打开的窗口
+
+示例：
+```
+const img=new imgZoom()
+
+img.close()
+```
+
+# 兼容性
+
+目前 IE11+ ,手机端目前也未兼容，后面再考虑向后兼容。
